7aeccb1c8c7ffda37922c6a225f8b1aa3600f8c0
Valdeir <valdeir904@gmail.com>
2026-09-21 11:20:18 -0300

    Add serverExternalPackages to fix Vercel build

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  serverExternalPackages: ['sqlite3', 'sqlite', 'mysql2', 'bcryptjs', '@vercel/postgres'],
   images: {
     remotePatterns: [
       {
