f4254f7941d384c04c87fb6d28d67cc2611db8d7
Valdeir <valdeir904@gmail.com>
2026-09-23 14:35:29 -0300

    fix: remove native scroll-behavior smooth to fix lenis stutter conflict

--- src/components/SmoothScroll.tsx
+++ src/components/SmoothScroll.tsx
@@ -5,7 +5,7 @@ import { ReactLenis } from '@studio-freight/react-lenis';
 
 export default function SmoothScroll({ children }: { children: React.ReactNode }) {
   return (
-    <ReactLenis root options={{ lerp: 0.08, duration: 1.2, smoothWheel: true }}>
+    <ReactLenis root options={{ lerp: 0.1, duration: 1.2, smoothWheel: true }}>
       {/* @ts-ignore - mismatch version of types/react */}
       {children}
     </ReactLenis>
