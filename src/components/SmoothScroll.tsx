5379c740d7b68e0e8896d37b407a44310cea0e11
Valdeir <valdeir904@gmail.com>
2026-09-23 14:30:13 -0300

    style: balance smooth scroll intensity

--- src/components/SmoothScroll.tsx
+++ src/components/SmoothScroll.tsx
@@ -5,7 +5,7 @@ import { ReactLenis } from '@studio-freight/react-lenis';
 
 export default function SmoothScroll({ children }: { children: React.ReactNode }) {
   return (
-    <ReactLenis root options={{ lerp: 0.04, duration: 1.5, smoothWheel: true, wheelMultiplier: 0.8 }}>
+    <ReactLenis root options={{ lerp: 0.08, duration: 1.2, smoothWheel: true }}>
       {/* @ts-ignore - mismatch version of types/react */}
       {children}
     </ReactLenis>
