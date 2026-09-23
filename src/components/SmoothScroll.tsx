750be765fc15c37bfc38fb3b04c15524c60fb69d
Valdeir <valdeir904@gmail.com>
2026-09-23 14:27:24 -0300

    fix: resolve React 19 type mismatch in lenis children

--- src/components/SmoothScroll.tsx
+++ src/components/SmoothScroll.tsx
@@ -6,6 +6,7 @@ import { ReactLenis } from '@studio-freight/react-lenis';
 export default function SmoothScroll({ children }: { children: React.ReactNode }) {
   return (
     <ReactLenis root options={{ lerp: 0.04, duration: 1.5, smoothWheel: true, wheelMultiplier: 0.8 }}>
+      {/* @ts-ignore - mismatch version of types/react */}
       {children}
     </ReactLenis>
   );
